582dac6623142df1de0c916a84d4880c21656309
EC2 Default User <ec2-user@ip-172-31-61-0.ec2.internal>
2021-11-11 15:24:50 +0000

    [Add]4-10

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+ protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+
+　# 新規登録後とログイン後のリダイレクト先
+  # def after_sign_in_path_for(resource)
+  #   users_path
+  # end
+
+  # def after_sign_in_path_for(resource)
+  # users_path
+  # end
+
 end
